13d4293492da80f4b4a2f55aa629a004a5c73c58
TeacherArea <sewilius.berg@initialize.se>
2026-01-21 07:18:14 +0100

    Added JsonStudentStorageService

---
+++ PRG2_MAUI_MVVM_Binding/Service/IStudentStorageService.cs
@@ -0,0 +1,11 @@
+﻿using PRG2_MAUI_MVVM_Binding.Model;
+
+namespace PRG2_MAUI_MVVM_Binding.Services
+{
+    // poängen med detta interface är att vi enkelt ska kunna byta Json till något annat, som SQLite
+    internal interface IStudentStorageService
+    {
+        Task SaveAsync(IEnumerable<Student> students);
+        Task<IList<Student>> LoadAsync();
+    }
+}
---
+++ PRG2_MAUI_MVVM_Binding/Service/JsonStudentStorageService.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using PRG2_MAUI_MVVM_Binding.Model;
+
+namespace PRG2_MAUI_MVVM_Binding.Services
+{
+    internal class JsonStudentStorageService : IStudentStorageService
+    {
+        private readonly string _filePath;
+
+        public JsonStudentStorageService()
+        {
+            _filePath = Path.Combine(
+                FileSystem.AppDataDirectory,
+                "students.json");
+        }
+
+        public async Task SaveAsync(IEnumerable<Student> students)
+        {
+            var json = JsonSerializer.Serialize(students,
+                new JsonSerializerOptions { WriteIndented = true });
+
+            await File.WriteAllTextAsync(_filePath, json);
+        }
+
+        public async Task<IList<Student>> LoadAsync()
+        {
+            if (!File.Exists(_filePath))
+                return new List<Student>();
+
+            var json = await File.ReadAllTextAsync(_filePath);
+            return JsonSerializer.Deserialize<List<Student>>(json)
+                   ?? new List<Student>();
+        }
+    }
+}
